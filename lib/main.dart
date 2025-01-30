2f0e394cef9933653b31d4dfa156665f9593334a
Santo Rasu <santorasu09.com>
2025-01-30 20:29:37 +0600

    Learn Themedata and GridView

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,20 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+      primaryColor: Colors.red,
+          appBarTheme: AppBarTheme(
+            color: Colors.red,
+            centerTitle: true
+          ),
+        scaffoldBackgroundColor: Colors.grey,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white
+          )
+        )
+      ),
       title: 'Flutter Class 2',
       home: Home(),
     );
@@ -25,7 +39,7 @@ class Home extends StatelessWidget {
     final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
+
         title: Column(
           children: [
             Text("Ostad Flutter 2",style: TextStyle(
@@ -33,7 +47,7 @@ class Home extends StatelessWidget {
             ),),
           ],
         ),
-        centerTitle: true,
+
       ),
       body: Column(
         children: [
@@ -66,8 +80,9 @@ class Home extends StatelessWidget {
                   ),
                   validator: (value){
                     if(value == null || value.isEmpty || value.length <6){
-
+                      return "Password must be 6 Character";
                     }
+                    return null;
                   },
                 ),
 
@@ -79,10 +94,35 @@ class Home extends StatelessWidget {
                         backgroundColor: Colors.green
                       ),
                       onPressed: (){
+                        if(_formKey.currentState!.validate()){
+                            Navigator.push(context,MaterialPageRoute(builder: (context)=>UserInfo()));
+                        }
                     print("Submit");
                   }, child: Text("Submit",style: TextStyle(
                     fontSize: 20, color: Colors.white,fontWeight: FontWeight.bold
                   ),)),
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 300,
+                  child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,crossAxisSpacing: 20,mainAxisSpacing: 20),
+                      itemCount: 9,
+                      itemBuilder: (context,index){
+                        return Container(
+                         decoration: BoxDecoration(
+                           color: Colors.orange,
+                           borderRadius: BorderRadius.circular(10)
+                         ),
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.person,size: 40,color: Colors.white,),
+                              ],
+                            ),
+                          )
+                        );
+                      })
                 )
               ],
             ),
@@ -92,6 +132,50 @@ class Home extends StatelessWidget {
       floatingActionButton: FloatingActionButton.extended(onPressed: (){
         print("Floating Action Button");
       }, label: Text("Add New")),
+    );
+  }
+}
+
+class UserInfo extends StatelessWidget {
+  const UserInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("User Information",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: Text("Back")),
+          ElevatedButton(onPressed: (){
+            Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>Page1()));
+          }, child: Text("Page 1"))
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("This is page 1",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: Text("Back")),
+        ],
+      ),
     );
   }
 }
